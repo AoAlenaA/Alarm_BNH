42b103c917be8b001d26a277fadc72e7b714ffe2
Alena <aoanuchina@edu.hse.ru>
2025-02-09 22:35:25 +0500

    hello

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Пора вставать.</Text>
+      <Text>Пора вставать на Гитхабе!!!!.</Text>
     </View>
   );
 }
