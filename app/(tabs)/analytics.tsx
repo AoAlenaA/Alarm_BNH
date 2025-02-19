3223ae540c5c419f61544964e46645472c9cae6d
veronikaKharit <113134807+veronikaKharit@users.noreply.github.com>
2025-02-19 22:50:43 +0500

    добавила аналитику

--- app/(tabs)/analytics.tsx
+++ app/(tabs)/analytics.tsx
@@ -3,7 +3,7 @@ import { Text, View,  StyleSheet } from 'react-native';
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>verra</Text>
+      <Text style={styles.text}>Аналитика</Text>
     </View>
   );
 }
